5f67af61547c05c0214b4c36362cd7a32752e2a6
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2019-12-31 16:49:00 -0700

    Add JavaScript and HTML Exercises
    Added the files that I'm working on involving HTML and JavaScript combined.

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+
+
+/*
+var sum = add(5,7);
+function add(num1,num2) {
+	return num1 + num2;
+}
+
+student.gradeAverage(3.4);
+
+// Note that a function is just a set of instructions that can be used when called.
+// A method is a function that an OBJECT can perform.  Ex:  Above, the student used the gradeAverage method.
+
+var student = {
+	firstname: "Jane";
+	lastName: "Smith";
+	age: 28;
+	gradeAverage: function(avg) { //This function needs and input.  This function is a method
+		return avg;
+	}
+}; //Notic the semicolon
+
+document.getElementById("Header_1").innerHTML */
